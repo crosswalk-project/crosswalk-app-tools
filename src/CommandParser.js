ac246d41ba2991d644f17b8f31d090a02d85da56
Robert Staudinger <robert.staudinger@intel.com>
2014-10-31 09:50:01 +0100

    CommandParser: Implement and document

--- src/CommandParser.js
+++ src/CommandParser.js
@@ -4,8 +4,16 @@
 
 var Console = require("./Console");
 
+/**
+ * Parsing and validation of command-line arguments.
+ * @constructor
+ */
 function CommandParser() {}
 
+/**
+ * Get primary command.
+ * @memberOf CommandParser
+ */
 CommandParser.prototype.getCommand = function() {
 
     var command = process.argv[2];
@@ -17,6 +25,10 @@ CommandParser.prototype.getCommand = function() {
     return null;
 };
 
+/**
+ * Get package name when command is "create".
+ * @memberOf CommandParser
+ */
 CommandParser.prototype.getCreatePackage = function() {
 
     var errormsg = "Invalid package name, see http://developer.android.com/guide/topics/manifest/manifest-element.html#package";
@@ -48,14 +60,48 @@ CommandParser.prototype.getCreatePackage = function() {
     return pkg;
 };
 
+/**
+ * Get version when command is "update".
+ * @memberOf CommandParser
+ */
 CommandParser.prototype.getUpdateVersion = function() {
 
-    // TODO
+    var errormsg = "Version must be of format ab.cd.ef.gh";
+
+    var version = process.argv[3];
+    var match = pkg.match("[0-9\.]*");
+    if (match[0] != version) {
+        Console.error(errormsg);
+        return null;
+    }
+    
+    var parts = version.split('.');
+    if (parts.length != 4) {
+        Console.error(errormsg);
+        return null;
+    }
+
+    return version;
 };
 
+/**
+ * Get build type when command is "build".
+ * @memberOf CommandParser
+ */
 CommandParser.prototype.getBuildType = function() {
 
-    // TODO
+    // Default to "debug" when no type given.
+    if (process.argv.length < 4) {
+        return "debug";
+    }
+
+    // Check build type is recognized.
+    var type = process.argv[3];
+    if (["debug", "release"].indexOf(type) > -1) {
+        return type;
+    }
+    
+    return null;
 };
 
 module.exports = CommandParser;
