15a574b92825d7a30f0116044aca5ac49c272e49
Robert Staudinger <robert.staudinger@intel.com>
2014-11-03 15:40:01 +0100

    test: Add CommandParser tests for "create", "update", "build"

--- test/command-parser.js
+++ test/command-parser.js
@@ -46,5 +46,106 @@ exports.tests = {
         test.equal(commands.indexOf(cmd), -1);
 
         test.done();
+    },
+
+    createGetPackage: function(test) {
+
+        test.expect(4);
+
+        // Good test
+        var argv1 = ["node", "foo", "create", "com.example.Bar"];
+        var cp1 = new CommandParser(argv1);
+
+        var cmd1 = cp1.getCommand();
+        test.equal(cmd1, argv1[2]);
+
+        var pkg1 = cp1.createGetPackage();
+        test.equal(pkg1, argv1[3]);
+
+        // Bad test, invalid package name
+        var argv2 = ["node", "foo", "create", "com.exam ple.Bar"];
+        var cp2 = new CommandParser(argv2);
+
+        var cmd2 = cp2.getCommand();
+        test.equal(cmd2, argv2[2]);
+
+        var pkg2 = cp2.createGetPackage();
+        test.equal(pkg2, null);
+
+        test.done();
+    },
+
+    updateGetVersion: function(test) {
+
+        test.expect(4);
+
+        // Good test
+        var argv1 = ["node", "foo", "update", "12.34.56.78"];
+        var cp1 = new CommandParser(argv1);
+
+        var cmd1 = cp1.getCommand();
+        test.equal(cmd1, argv1[2]);
+
+        var version1 = cp1.updateGetVersion();
+        test.equal(version1, argv1[3]);
+
+        // Bad test, invalid version
+        var argv2 = ["node", "foo", "update", "12.34.x6.78"];
+        var cp2 = new CommandParser(argv2);
+
+        var cmd2 = cp2.getCommand();
+        test.equal(cmd2, argv2[2]);
+
+        var version2 = cp2.updateGetVersion();
+        test.equal(version2, null);
+
+        test.done();
+    },
+
+    buildGetType: function(test) {
+
+        test.expect(8);
+
+        // Good test, default build "debug"
+        var argv1 = ["node", "foo", "build"];
+        var cp1 = new CommandParser(argv1);
+
+        var cmd1 = cp1.getCommand();
+        test.equal(cmd1, argv1[2]);
+
+        var type1 = cp1.buildGetType();
+        test.equal(type1, "debug");
+
+        // Good test, build "debug"
+        var argv2 = ["node", "foo", "build", "debug"];
+        var cp2 = new CommandParser(argv2);
+
+        var cmd2 = cp2.getCommand();
+        test.equal(cmd2, argv2[2]);
+
+        var type2 = cp2.buildGetType();
+        test.equal(type2, argv2[3]);
+
+        // Good test, build "release"
+        var argv3 = ["node", "foo", "build", "release"];
+        var cp3 = new CommandParser(argv3);
+
+        var cmd3 = cp3.getCommand();
+        test.equal(cmd3, argv3[2]);
+
+        var type3 = cp3.buildGetType();
+        test.equal(type3, "release");
+
+        // Bad test, unknown type
+        var argv4 = ["node", "foo", "build", "foo"];
+        var cp4 = new CommandParser(argv4);
+
+        var cmd4 = cp4.getCommand();
+        test.equal(cmd4, argv4[2]);
+
+        var type4 = cp4.buildGetType();
+        test.equal(type4, null);
+
+        test.done();
     }
 };
