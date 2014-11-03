5576676602a14d68f9d63f212d247695b13081d6
Robert Staudinger <robert.staudinger@intel.com>
2014-11-03 20:23:35 +0100

    ConsoleIface: New interface to wrap logging

---
+++ src/ConsoleIface.js
@@ -0,0 +1,32 @@
+// Copyright © 2014 Intel Corporation. All rights reserved.
+// Use  of this  source  code is  governed by  an Apache v2
+// license that can be found in the LICENSE-APACHE-V2 file.
+
+/**
+ * Console output interface.
+ * @constructor
+ * @protected
+ */
+function ConsoleIface() {}
+
+/**
+ * Log error message.
+ * @param {String} message
+ * @memberOf ConsoleIface
+ */
+ConsoleIface.prototype.error = function(message) {
+
+    throw new Error("ConsoleIface.error() not implemented.");
+};
+
+/**
+ * Log warning message.
+ * @param {String} message
+ * @memberOf ConsoleIface
+ */
+ConsoleIface.prototype.warning = function(message) {
+
+    throw new Error("ConsoleIface.warning() not implemented.");
+};
+
+module.exports = ConsoleIface;
