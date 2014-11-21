f65d1eb3f1f470e1a997499b81eaf39dd9acda5f
Robert Staudinger <robert.staudinger@intel.com>
2014-11-21 13:57:05 +0100

    Project: Refactor so JSDoc understands it better
    Apparently JSDoc understands the constructor + prototype methods
    way of defining classes better, than the object-notation one. So
    use the former, for being able to cross-reference docs.

--- src/Project.js
+++ src/Project.js
@@ -4,52 +4,59 @@
 
 /**
  * Callback signature for {@link Project.generate}
- * @function generateCb
+ * @function projectOperationCb
  * @param {String} errormsg null on success, otherwise error message.
  * @memberOf Project
  * @inner
  */
-function projectCb(result) {}
+function projectOperationCb(result) {}
 
 /**
  * Interface for project implementations.
+ * @constructor
+ * @protected
  */
-var Project = {
+function Project() {}
 
-    /**
-     * Generate project template.
-     * @function generate
-     * @param {String} packageId Package name in com.example.Foo format.
-     * @param {Function} callback see {@link Project~projectCb}.
-     * @abstract
-     */
-    generate: function(packageId, callback) {
+/**
+ * Generate project template.
+ * @function generate
+ * @param {String} packageId Package name in com.example.Foo format.
+ * @param {Function} callback see {@link Project~projectOperationCb}.
+ * @abstract
+ * @memberOf Project
+ */
+Project.prototype.generate =
+function(packageId, callback) {
 
-        throw new Error("Project.generate() not implemented.");
-    },
-
-    update: function() {
-
-        throw new Error("Project.update() not implemented.");
-    },
-
-    refresh: function() {
-
-        throw new Error("Project.refresh() not implemented.");
-    },
-
-    /**
-     * Build application package.
-     * @function build
-     * @param {String[]} abi Array of ABIs, supported armeabi-v7a, x86.
-     * @param {Boolean} release Whether to build debug or release package.
-     * @param {Function} callback see {@link Project~projectCb}.
-     * @abstract
-     */
-    build: function(abis, release, callback) {
-
-        throw new Error("Project.build() not implemented.");
-    }
+    throw new Error("Project.generate() not implemented.");
 };
 
-module.exports = Project;
+Project.prototype.update =
+function() {
+
+    throw new Error("Project.update() not implemented.");
+};
+
+Project.prototype.refresh =
+function() {
+
+    throw new Error("Project.refresh() not implemented.");
+};
+
+/**
+ * Build application package.
+ * @function build
+ * @param {String[]} abi Array of ABIs, supported armeabi-v7a, x86.
+ * @param {Boolean} release Whether to build debug or release package.
+ * @param {Function} callback see {@link Project~projectOperationCb}.
+ * @abstract
+ * @memberOf Project
+ */
+Project.prototype.build =
+function(abis, release, callback) {
+
+    throw new Error("Project.build() not implemented.");
+};
+
+module.exports = new Project();
