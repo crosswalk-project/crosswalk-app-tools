f5ed000d6ad3ce6db271cbdd866ffebc61e6fba6
Robert Staudinger <robert.staudinger@intel.com>
2014-12-12 14:53:30 +0100

    IndexParser: Implement new class for parsing html index pages
    This is a bit of a hack surely, but it only needs to work against
    our own download site.

---
+++ src/IndexParser.js
@@ -0,0 +1,52 @@
+// Copyright © 2014 Intel Corporation. All rights reserved.
+// Use  of this  source  code is  governed by  an Apache v2
+// license that can be found in the LICENSE-APACHE-V2 file.
+
+var Config = require("./Config");
+
+/**
+ * Create an IndexParser object.
+ * This is used to determine available versions from the Crosswalk download page.
+ * @param {String} data Index page data.
+ * @constructor
+ */
+function IndexParser(data) {
+
+    this._data = data;
+}
+
+/**
+ * Parse page and return available versions (subdirectories).
+ * @function parse
+ * @returns {String[]} Array of versions in w.x.y.z format.
+ * @memberOf IndexParser
+ */
+IndexParser.prototype.parse =
+function() {
+
+    var parentDirRegex = /Parent Directory/i;
+
+    // ignore all lines until we get to the first one after
+    // "Parent Directory"
+    var lines = this._data.split('\n');
+    var versions = [];
+
+    for (var i = 0; i < lines.length; i += 1) {
+
+        var line = lines[i];
+        if (parentDirRegex.test(line)) {
+            continue;
+        }
+
+        var dirPrefix = '<img src="/icons/folder.gif" alt="[DIR]"> <a href="';
+        if (line.substring(0, dirPrefix.length) == dirPrefix) {
+
+            var version = line.substring(dirPrefix.length, line.indexOf('/"'));
+            versions.push(version);
+        }
+    }
+
+    return versions;
+};
+
+module.exports = IndexParser;
