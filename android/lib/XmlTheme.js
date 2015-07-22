9c5a85f8d96e11a057e9629640bb2fc73f76c883
Robert Staudinger <robert.staudinger@intel.com>
2015-07-22 14:01:27 +0200

    XmlTheme: New class for handling xml theme manipulation
    Implement handling of the "fullscreen" theme property.
    
    Also add test case.

---
+++ android/lib/XmlTheme.js
@@ -0,0 +1,107 @@
+// Copyright © 2014 Intel Corporation. All rights reserved.
+// Use  of this  source  code is  governed by  an Apache v2
+// license that can be found in the LICENSE-APACHE-V2 file.
+
+var FS = require("fs");
+
+var xmldom = require("xmldom");
+
+/**
+ * Wrapper for theme.xml.
+ * @param {OutputIface} output Output implementation
+ * @param {String} path Path to manifest.json
+ */
+function XmlTheme(output, path) {
+
+    this._output = output;
+    this._path = path;
+
+    var doc = this.read();
+
+    this._fullscreen = false;
+    var node = this.findItem(doc, "android:windowFullscreen");
+    if (node) {
+        this._fullscreen = node.textContent === "true";
+    } else {
+        this._output.error("Failed to find 'fullscreen' setting in " + this._path);
+    }
+}
+
+/**
+ * Fullscreen mode
+ * @member {Boolean} fullscreen
+ * @instance
+ * @memberOf XmlTheme
+ */
+Object.defineProperty(XmlTheme.prototype, "fullscreen", {
+                      get: function() {
+                                return this._fullscreen;
+                           },
+                      set: function(fullscreen) {
+                                this._fullscreen = typeof fullscreen === "boolean" && fullscreen;
+
+                                var doc = this.read();
+                                var node = this.findItem(doc, "android:windowFullscreen");
+                                if (node) {
+                                    node.textContent = this._fullscreen ? "true" : "false";
+                                    this.write(doc);
+                                } else {
+                                    this._output.error("Failed to find 'fullscreen' setting in " + this._path);
+                                }
+                           }
+                      });
+
+/**
+ * Read theme.xml
+ * @returns {xmldom.Document} XML Document
+ * @private
+ * @see {@link https://github.com/jindw/xmldom}
+ */
+XmlTheme.prototype.read =
+function() {
+
+    // TODO Error handling
+
+    var parser = new xmldom.DOMParser();
+    var buf = FS.readFileSync(this._path, {"encoding": "utf8"});
+    return parser.parseFromString(buf);
+};
+
+/**
+ * Write theme.xml
+ * @param {xmldom.Document} doc XML Document
+ * @private
+ * @see {@link https://github.com/jindw/xmldom}
+ */
+XmlTheme.prototype.write =
+function(doc) {
+
+    // TODO Error handling
+
+    var serializer = new xmldom.XMLSerializer();
+    var buf = serializer.serializeToString(doc);
+    FS.writeFileSync(this._path, buf);
+};
+
+/**
+ * Find fullscreen node
+ * @param {xmldom.Document} document
+ * @returns {xmldom.Node} Node if found or null
+ * @private
+ */
+XmlTheme.prototype.findItem =
+function(doc, name) {
+
+    var node = null;
+
+    items = doc.getElementsByTagName("item");
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].getAttribute("name") === name) {
+            return items[i];
+        }
+    }
+
+    return null;
+};
+
+module.exports = XmlTheme;
---
+++ android/test/xml-theme.js
@@ -0,0 +1,60 @@
+// Copyright © 2014 Intel Corporation. All rights reserved.
+// Use  of this  source  code is  governed by  an Apache v2
+// license that can be found in the LICENSE-APACHE-V2 file.
+
+var FS = require("fs");
+
+var ShellJS = require("shelljs");
+
+var XmlTheme = require("../lib/XmlTheme.js");
+var Util = require("../../test-util/Util.js");
+
+var _output = require("../../src/TerminalOutput").getInstance();
+
+
+
+function createTheme() {
+
+    var path = Util.createTmpFile();
+    var content = '' +
+'<?xml version="1.0" encoding="utf-8"?>\n' +
+'\n' +
+'<!--\n' +
+'Copyright (c) 2014 Intel Corporation. All rights reserved.\n' +
+'Use of this source code is governed by a BSD-style license that can be\n' +
+'found in the LICENSE file.\n' +
+'-->\n' +
+'\n' +
+'<resources>\n' +
+'    <!-- Application theme. -->\n' +
+'    <style name="AppTheme" parent="@android:style/Theme.Holo.Light.NoActionBar">\n' +
+'        <item name="android:windowFullscreen">false</item>\n' +
+'        <item name="android:windowBackground">@null</item>\n' +
+'    </style>\n' +
+'</resources>\n';
+
+    FS.writeFileSync(path, content);
+
+    return path;
+}
+
+exports.tests = {
+
+    fullscreen: function(test) {
+
+        test.expect(2);
+
+        var path = createTheme();
+        var theme = new XmlTheme(_output, path);
+        test.equal(theme.fullscreen, false);
+
+        // change and read back
+        theme.fullscreen = true;
+        var theme = new XmlTheme(_output, path);
+        test.equal(theme.fullscreen, true);
+
+        ShellJS.rm("-f", path);
+
+        test.done();
+    }
+};
